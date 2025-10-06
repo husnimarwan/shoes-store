50be1f50fdc2118c075fea7e7d84bfe1622607e2
Husni Marwan <wisu.tisu@gmail.com>
2025-10-06 18:46:20 +0700

    Add search bar turning into symbol feature for responsive layout

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,7 +12,10 @@ const Navigation = () => {
           <h1>Snickers</h1>
         </Link>
         <div className="search-container">
-          <input type="search" placeholder="Search for products..." />
+          <label htmlFor="search-input" className="search-icon-label">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
+          </label>
+          <input id="search-input" type="search" placeholder="Search for products..." />
         </div>
         <ul className="nav-menu">
           <li className="nav-item">
